98e1daf980918582533f299942da4a08d674e69d
Dimas Saputra <adityabariqikhsan@gmail.com>
2023-04-24 22:51:09 +0700

    Adding speed up power up

--- Assets/script/BallControl.cs
+++ Assets/script/BallControl.cs
@@ -30,4 +30,9 @@ public class BallControl : MonoBehaviour
         transform.position = new Vector3(resetPosition.x, resetPosition.y, 2);
         InitialSpeed();
     }
+
+    public void ActivatePUSpeedUp(float magnitude)
+    {
+        rig.velocity *= magnitude;
+    }
 }
---
+++ Assets/script/PUSpeedUpControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PUSpeedUpControl : MonoBehaviour
+{
+    public PowerUpManager manager;
+    public Collider2D ball;
+    public float magnitude = 2;
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if (collision == ball)
+        {
+            ball.GetComponent<BallControl>().ActivatePUSpeedUp(magnitude);
+            manager.RemovePowerUp(gameObject);
+        }    
+    }
+}
